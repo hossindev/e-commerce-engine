01eee734b1c6cfc5552dce8e01349f74febaa58f
ryzzlab <ryzzlab@fedora.fritz.box>
2026-06-11 19:58:58 +0200

    first commit

---
+++ src/main/java/com/ryzzlab/e_commerce_engine/ECommerceEngineApplication.java
@@ -0,0 +1,13 @@
+package com.ryzzlab.e_commerce_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryzzlab/e_commerce_engine/ECommerceEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryzzlab.e_commerce_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
