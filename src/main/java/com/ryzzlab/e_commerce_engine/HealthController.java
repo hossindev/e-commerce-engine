cf79572f0eb67f9114981aebd3aad9ee84fa7cc0
ryzzlab <ryzzlab@fedora.fritz.box>
2026-06-12 20:32:10 +0200

    health controller json

--- src/main/java/com/ryzzlab/e_commerce_engine/HealthController.java
+++ src/main/java/com/ryzzlab/e_commerce_engine/HealthController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/")
 public class HealthController {
     @GetMapping("health")
     public ResponseEntity<?> health(){
-        return ResponseEntity.ok().body("Status ok");
+        return ResponseEntity.ok(Map.of("status","ok"));
     }
 }
